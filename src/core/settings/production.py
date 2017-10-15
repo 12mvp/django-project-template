16de69ae45ba6431d53510232a0f00027d8a40f5
nezo <thibault.appourchaux@gmail.com>
2017-10-15 17:53:50 +0200

    Project template for Django 1.11.4

---
+++ src/core/settings/production.py
@@ -0,0 +1,74 @@
+import os
+import logging.config
+
+from .base import *
+
+
+# For security and performance reasons, DEBUG is turned off
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+
+# Must mention ALLOWED_HOSTS in production!
+ALLOWED_HOSTS = []
+
+
+# Cache the templates in memory for speed-up
+loaders = [
+    ('django.template.loaders.cached.Loader', [
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    ]),
+]
+
+TEMPLATES[0]['OPTIONS'].update({"loaders": loaders})
+TEMPLATES[0].update({"APP_DIRS": False})
+
+
+# Define STATIC_ROOT for the collectstatic command
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'site', 'static')
+
+
+# Log everything to the logs directory at the top
+LOGFILE_ROOT = os.path.join(dirname(BASE_DIR), 'logs')
+
+if not os.path.isdir(LOGFILE_ROOT):
+    os.makedirs(LOGFILE_ROOT)
+
+
+# Reset logging
+LOGGING_CONFIG = None
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(pathname)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'proj_log_file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(LOGFILE_ROOT, 'project.log'),
+            'formatter': 'verbose'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        }
+    },
+    'loggers': {
+        'project': {
+            'handlers': ['proj_log_file'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+logging.config.dictConfig(LOGGING)
